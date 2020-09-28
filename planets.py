67acbf73942c62a2c80e0c2f043c1a8b7bf02aae
Julian Garcia <garcia.j.julian@gmail.com>
2020-09-27 20:24:42 -0400

    uncommented examples

--- planets.py
+++ planets.py
@@ -1,17 +1,17 @@
-# planet_list = ["Mercury", "Venus", "Mars"]
+planet_list = ["Mercury", "Venus", "Mars"]
 
-# planet_list.append('Jupiter')
-# planet_list.append('Saturn')
+planet_list.append('Jupiter')
+planet_list.append('Saturn')
 
-# planet_list.extend(['Saturn', 'Uranus'])
+planet_list.extend(['Saturn', 'Uranus'])
 
-# planet_list.insert(3, 'Earth')
+planet_list.insert(3, 'Earth')
 
-# planet_list.append('Pluto')
+planet_list.append('Pluto')
 
-# rocky_planets = planet_list
+rocky_planets = planet_list
 
-# del rocky_planets[8]
+del rocky_planets[8]
 
 
 # Example spacecraft list
